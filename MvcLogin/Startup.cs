e0ffddc78dc7d91866b1d37466c53b53ed5ace3c
Jung Yi <busyweb@gmail.com>
2017-01-05 22:12:37 -0800

    Initial upload.

---
+++ MvcLogin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcLogin.Startup))]
+namespace MvcLogin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
